ced19dd821111ff10c285cacfc081c8771f6be92
Chayan Maji <chayanmaji@gmail.com>
2022-02-27 23:17:46 -0500

    Created the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Nav,Navbar, Container, FormControl, Dropdown, Badge } from 'react-bootstrap'
+import { FaShoppingCart } from 'react-icons/fa';
 
 function Header() {
   return (
-    <div>Header</div>
+    <Navbar bg='dark' variant='dark' style={{ height: 80 }}> 
+        <Container>
+            <Navbar.Brand>
+                <a href="/">Shoppin Cart</a>
+            </Navbar.Brand>
+            <Navbar.Text className='search'>
+                <FormControl 
+                    style={{ width:500 }}
+                    placeholder="Search a product"
+                    className="m-auto"
+                />
+            </Navbar.Text>
+            <Nav>
+                <Dropdown alignRight>
+                    <Dropdown.Toggle variant='success'>
+                        <FaShoppingCart className="ShoppingCart"/>
+                        <Badge>{10}</Badge>
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu style={{ minWidth:370 }}>
+                        <span style={{ padding:10 }}>Cart is empty</span>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Nav>
+        </Container>
+    </Navbar>
   )
 }
 
